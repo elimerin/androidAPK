8bdaef26837dcb42cb090a3f2e49b0add9b29b7b
elimerin <elimerin@github.com>
2017-09-03 17:21:34 +0200

    2

--- Pogodyn/app/src/main/java/com/example/ansh/pogodyn/AccuService.java
+++ Pogodyn/app/src/main/java/com/example/ansh/pogodyn/AccuService.java
@@ -2,12 +2,10 @@ package com.example.ansh.pogodyn;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
-/**
- * Created by ansh on 03.09.2017.
- */
 
 public interface AccuService {
-    @GET("locations/v1/cities/search?apikey=jPRAPCFDpaLuWPCyCVdNH0ayBXziM7QS&q={location}")
-    Call<locationKey>//za chuj nie wiem co tutaj ma byc
+    @GET("locations/v1/cities/search")
+    Call<String> loadCities(@Query("apikey") String apikey, @Query("q") String location);
 }
