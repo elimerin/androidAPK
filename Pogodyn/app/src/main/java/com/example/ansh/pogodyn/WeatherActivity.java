07d8551dce36537efbf19a3dba10854db9736314
elimerin <elimerin@github.com>
2017-09-10 21:15:23 +0200

    11

--- Pogodyn/app/src/main/java/com/example/ansh/pogodyn/WeatherActivity.java
+++ Pogodyn/app/src/main/java/com/example/ansh/pogodyn/WeatherActivity.java
@@ -12,9 +12,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * Created by ariankozbial on 09.09.2017.
- */
 
 public class WeatherActivity extends AppCompatActivity {
 
